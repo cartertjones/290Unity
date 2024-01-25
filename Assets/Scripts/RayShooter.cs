1f92c2ad82a496753a8bdfd992b942dbe367cc81
Carter Jones <carterjones353@u.boisestate.edu>
2024-01-24 21:43:58 -0700

    Added Unity game

---
+++ Assets/Scripts/RayShooter.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RayShooter : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] private TextMeshProUGUI hitPointUI;
+    [SerializeField] private GameObject bulletIndicator;
+
+    void Start() {
+        cam = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            Vector3 point = new Vector3(cam.pixelWidth/2, cam.pixelHeight/2, 0);
+            Ray ray = cam.ScreenPointToRay(point);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit)) {
+                GameObject hitObject = hit.transform.gameObject;
+                ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+                if(target != null) {
+                    target.ReactToHit();
+                    hitPointUI.text =  "Hit object at: " + hitObject.transform.position;
+                }
+                else {
+                    StartCoroutine(SphereIndicator(hit.point));
+                    hitPointUI.text = "";
+                }
+            }
+        }
+    }
+
+    private IEnumerator SphereIndicator(Vector3 pos) {
+        GameObject sphere = GameObject.Instantiate(bulletIndicator, pos, Quaternion.identity);
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
+}
